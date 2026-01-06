eb288ebd70a59b82215423211fe197d1dbb2464e
riyakanakparmar <external.kanak.parmar@riya.travel>
2026-01-06 12:15:05 +0530

    Commit merge

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,13 @@ use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
